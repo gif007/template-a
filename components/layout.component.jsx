fccc4d7e75637362771f0d70c8d7af0d41c42fec
bard <carruthers.n.t@gmail.com>
2021-12-28 21:48:04 -0400

    instantiated project

---
+++ components/layout.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/layout.module.scss'
+
+export default function Layout({children}) {
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import Layout from '../components/layout.component'
+import '../styles/reset.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
