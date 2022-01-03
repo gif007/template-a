aa28beeafb6ea38cbd4cdc351449a0a84035d518
bard <carruthers.n.t@gmail.com>
2022-01-02 22:18:09 -0400

    finished first draft of layout

--- components/layout.component.jsx
+++ components/layout.component.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import styles from '../styles/layout.module.scss'
+import utilStyles from '../styles/utils.module.scss'
 
 export default function Layout({children}) {
     return (
-        <div>
-            {children}
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <h1>Header</h1>
+            </header>
+            <div className={utilStyles.row}>
+                <aside className={styles.aside}>
+                    <h3>Nav menu</h3>
+                    <nav>
+                        <div>Link 1</div>
+                        <div>Link 2</div>
+                    </nav>
+                </aside>
+                <main className={styles.main}>{children}</main>
+            </div>
+            <footer className={styles.footer}>
+                <h4>Footer</h4>
+            </footer>
         </div>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,7 @@
-import Layout from '../components/layout.component'
 import '../styles/reset.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
